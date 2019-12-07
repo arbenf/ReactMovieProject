64b8d0d33ae3831a1b4807ab70f48ca54d186aac
arbenf <32666171+arbenf@users.noreply.github.com>
2019-12-07 22:23:08 +0100

    Added searchfield

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-import React, {Component} from 'react';
-import Movies from './components/Movies';
+import React, { Component } from "react";
+import Movies from "./components/Movies";
+import Search from "./components/Search";
+
 class App extends Component {
   state = {
     movies: [
@@ -7,17 +9,22 @@ class App extends Component {
       // {Poster: "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},
       // // {Poster: "https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg"}
     ]
-  }
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     fetch("https://www.omdbapi.com/?s=batman&apikey=7a3ecfb6")
-    .then(response => response.json())
-    .then(data => this.setState({movies: data.Search}))
-    .catch(error => console.log(error))
+      .then(response => response.json())
+      .then(data => this.setState({ movies: data.Search }))
+      .catch(error => console.log(error));
   }
 
-  render(){
-    return <Movies movies={this.state.movies}/>    
+  render() {
+    return (
+      <React.Fragment>
+        <Search />
+        <Movies movies={this.state.movies} />
+      </React.Fragment>
+    );
   }
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Search extends Component {
-  render(){
-    return <input type="text"></input>
+  state = {
+    title: ""
+  };
+  handleClick = () => {
+    console.log("Clicked");
+  };
+
+  searchMovie = event => {
+    console.log(event.target.value);
+    this.setState({ title: event.target.value });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleClick}>
+        <input
+          type="text"
+          value={this.state.title}
+          onChange={this.searchMovie}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    );
   }
 }
 

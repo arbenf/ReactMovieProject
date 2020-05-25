fc612eb10ef15a795fcc35d1caf1284878a5aa09
arbenf <32666171+arbenf@users.noreply.github.com>
2020-05-25 15:45:33 +0200

    Update movieReducer.js

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -30,31 +30,5 @@ const movieReducer = (state = initialState, action) => {
 
 export default movieReducer;
 
-import { FETCH_MOVIES, SEARCH_MOVIE } from "../actions/types";
 
-const initialState = {
-  items: [],
-  item: {}
-};
-
-const movieReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_MOVIES:
-      return {
-        ...state,
-        items: action.payload
-      };
-    case SEARCH_MOVIE:
-      return {
-        ...state,
-        items: action.payload
-
-
-      };
-    default:
-      return state;
-  }
-};
-
-export default movieReducer;
 

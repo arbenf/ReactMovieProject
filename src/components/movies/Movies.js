3c7e97eaf5ead7d280602cf11f32cdeb3ea4f03b
arbenf <32666171+arbenf@users.noreply.github.com>
2020-05-26 12:52:44 +0200

    Update Movies.js

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -10,51 +10,6 @@ import styles from "./movies.module.css";
 class Movies extends Component {
   componentDidMount() {
     this.props.fetchMovies();
-    console.log("Movies componentDidMount", this.props.movies);
-  }
-  // shouldComponentUpdate() {
-  //   console.log("Movies shouldComponentUpdate");
-  //   return true;
-  // }
-  render() {
-    return (
-      console.log("Movies render", this.props.movies),
-      (
-        <div className={styles.movies}>
-          {this.props.movies.map(movie => (
-            <Movie movie={movie} key={movie.imdbID} />
-          ))}
-        </div>
-      )
-    );
-  }
-}
-
-Movies.propTypes = {
-  fetchMovies: PropTypes.func.isRequired,
-  movies: PropTypes.array.isRequired
-};
-
-const mapStateToProps = state => ({
-  movies: state.movies.items
-});
-
-export default connect(
-  mapStateToProps,
-  { fetchMovies }
-)(Movies);
-import React, { Component } from "react";
-import Movie from "../movie/Movie";
-import PropTypes from "prop-types";
-
-import { connect } from "react-redux";
-import { fetchMovies } from "../../actions/movieActions";
-
-import styles from "./movies.module.css";
-
-class Movies extends Component {
-  componentDidMount() {
-    this.props.fetchMovies();
   }
 
   render() {

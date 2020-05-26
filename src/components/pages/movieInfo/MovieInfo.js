e7fea170706a5d9650697054cf6249670f5c1b15
arbenf <32666171+arbenf@users.noreply.github.com>
2020-05-26 12:55:44 +0200

    Update MovieInfo.js

--- src/components/pages/movieInfo/MovieInfo.js
+++ src/components/pages/movieInfo/MovieInfo.js
@@ -2,64 +2,6 @@ import React, { Component } from "react";
 import Loading from "../../../components/loading/Loading";
 import PropTypes from "prop-types";
 import { movieInfo } from "../../../actions/movieActions";
-
-import { connect } from "react-redux";
-
-import styles from "./movieInfo.module.css";
-
-class MovieInfo extends Component {
-  state = {
-    loading: true
-  };
-
-  componentDidMount() {
-    //Wait until data is fetched
-    setTimeout(() => {
-      this.setState({ loading: false });
-    }, 1000);
-  }
-
-  render() {
-    console.log("movieInfo render", this.props.movie.Title);
-
-    const { Poster, Title, Plot } = this.props.movie;
-
-    if (this.state.loading) {
-      return <Loading />;
-    }
-    // console.log(this.props.movie.Title);
-    return (
-      <div className={styles.movieInfo}>
-        <img
-          className={styles.image}
-          src={Poster}
-          width="400"
-          height="550"
-          alt="moviePoster"
-        />
-        <p>{Title}</p>
-        <p>{Plot}</p>
-      </div>
-    );
-  }
-}
-
-MovieInfo.propTypes = {
-  movieInfo: PropTypes.func.isRequired
-};
-
-const mapStateToProps = state => ({
-  movie: state.movies.item
-});
-
-export default connect(
-  mapStateToProps,
-  { movieInfo }
-)(MovieInfo);
-import React, { Component } from "react";
-import Loading from "../../../components/loading/Loading";
-import PropTypes from "prop-types";
-import { movieInfo } from "../../../actions/movieActions";
 import Movies from "../../movies/Movies";
 
 import { connect } from "react-redux";

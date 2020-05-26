f4ef77abf2699beb85206880773fcc5e5c9cbd6d
arbenf <32666171+arbenf@users.noreply.github.com>
2020-05-26 12:40:31 +0200

    Update Loading.js

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -14,19 +14,3 @@ function loading() {
 }
 
 export default loading;
-import React from "react";
-
-import styles from "./loading.module.css";
-
-function loading() {
-  return (
-    <div className={styles.loading}>
-      <div />
-      <div />
-      <div />
-      <div />
-    </div>
-  );
-}
-
-export default loading;

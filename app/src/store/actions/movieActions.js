3e1e8e8a57a30363308a0e8125bd72f735cbb910
Arben Fazlioski <arbenf@hotmail.se>
2021-03-06 22:38:30 +0100

    Added baseUrl variable

--- app/src/store/actions/movieActions.js
+++ app/src/store/actions/movieActions.js
@@ -1,10 +1,11 @@
 import * as actionTypes from "./types";
 
 const API_KEY = "6a63a37017cbb2d7c209d1bdafb1f600";
+const baseUrl = "https://api.themoviedb.org/3/";
 
 export const fetchMovies = () => {
   return (dispatch) => {
-    fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
+    fetch(`${baseUrl}trending/movie/day?api_key=${API_KEY}`)
       .then((response) => response.json())
       .then((data) =>
         dispatch({
@@ -19,7 +20,7 @@ export const fetchMovies = () => {
 export const searchMovie = (title) => (dispatch) => {
   console.log("searchTitle inside Redux: ", title);
   fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${title}&page=1&include_adult=false`
+    `${baseUrl}search/movie?api_key=${API_KEY}&language=en-US&query=${title}&page=1&include_adult=false`
   )
     .then((response) => response.json())
     .then((data) =>
@@ -33,7 +34,7 @@ export const searchMovie = (title) => (dispatch) => {
 
 export const movieInfo = (id) => (dispatch) => {
   fetch(
-    `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
+    `${baseUrl}movie/${id}?api_key=${API_KEY}&language=en-US`
   )
     .then((res) => res.json())
     .then((data) =>
@@ -46,7 +47,7 @@ export const movieInfo = (id) => (dispatch) => {
 };
 
 export const credits = (id) => (dispatch) => {
-  fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}
+  fetch(`${baseUrl}movie/${id}/credits?api_key=${API_KEY}
   `)
     .then((res) => res.json())
     .then((data) =>

051f9f7f91210f2070fbdd1c12b85d2b2c17a780
arbenf <32666171+arbenf@users.noreply.github.com>
2021-03-10 10:01:54 +0100

    Added TMDb logo

--- app/src/components/header/Header.js
+++ app/src/components/header/Header.js
@@ -6,9 +6,18 @@ import styles from "./header.module.css";
 function header() {
   return (
     <header className={styles.header}>
+      <img
+        src={
+          "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
+        }
+        width="100"
+        height="150"
+        alt="logo"
+      />
       <Link className={styles.headerLink} to="/">
         Movie Finder
       </Link>
+      <div className={styles.div} />
     </header>
   );
 }

f4d3a292a9d64bf1fc682463172b93c9e188629a
Arben Fazlioski <arbenf@hotmail.se>
2021-05-24 14:22:19 +0200

    header dropdown menu and hide on scroll

--- app/src/components/header/Header.js
+++ app/src/components/header/Header.js
@@ -1,55 +1,80 @@
-import React from "react";
+import React, { Component } from "react";
 import Search from "../search/Search";
 import { Link } from "react-router-dom";
 
 import styles from "./header.module.css";
 
-function header() {
-  return (
-    <header className={styles.header}>
-      <nav>
-        <ul>
-          <li className={styles.logo}>
-            <Link className={styles.headerLink} to="/">
-              The Movie Finder
-            </Link>
-          </li>
-          <div className={styles.dropdown}>
-            <li className={styles.genres}>Movies</li>
-            <div className={styles.dropdownContent}>
-              <a href="#">Popular</a>
-              <a href="#">Top 100</a>
-              <a href="#">New releases</a>
-            </div>
-          </div>
-          <div className={styles.dropdown}>
-            <li className={styles.genres}>Tv Shows</li>
-            <div className={styles.dropdownContent}>
-              <a href="#">Popular</a>
-              <a href="#">Top 100</a>
-              <a href="#">New releases</a>
-            </div>
-          </div>
-          <li className={styles.fourth}>
-            <Search />
-          </li>
-        </ul>
-      </nav>
+class Header extends Component {
+  state = {
+    show: true,
+    scrollPos: 0,
+  };
 
-      {/* <img
-        src={
-          "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
-        }
-        width="100"
-        height="150"
-        alt="logo"
-      />
-      <Link className={styles.headerLink} to="/">
-        Movie Finder
-      </Link>
-      <div className={styles.div} /> */}
-    </header>
-  );
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+  handleScroll = () => {
+    // console.log(document.body.getBoundingClientRect());
+    this.setState({
+      scrollPos: document.body.getBoundingClientRect().top,
+      show: document.body.getBoundingClientRect().top > this.state.scrollPos,
+    });
+  };
+  // render() {
+  //   console.log(this.state);
+  //   return <nav className={this.state.show ? "active" : "hidden"}>Navbar</nav>;
+  // }
+
+  render() {
+    return (
+      <header className={this.state.show ? styles.active : styles.hidden}>
+        <nav>
+          <ul>
+            <li className={styles.logo}>
+              <Link className={styles.headerLink} to="/">
+                The Movie Finder
+              </Link>
+            </li>
+            <div className={styles.dropdown}>
+              <li className={styles.genres}>Movies</li>
+              <div className={styles.dropdownContent}>
+                <a href="#">Popular</a>
+                <a href="#">Top 100</a>
+                <a href="#">New releases</a>
+              </div>
+            </div>
+            <div className={styles.dropdown}>
+              <li className={styles.genres}>Tv Shows</li>
+              <div className={styles.dropdownContent}>
+                <a href="#">Popular</a>
+                <a href="#">Top 100</a>
+                <a href="#">New releases</a>
+              </div>
+            </div>
+            <li className={styles.fourth}>
+              <Search />
+            </li>
+          </ul>
+        </nav>
+
+        {/* <img
+          src={
+            "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
+          }
+          width="100"
+          height="150"
+          alt="logo"
+        />
+        <Link className={styles.headerLink} to="/">
+          Movie Finder
+        </Link>
+        <div className={styles.div} /> */}
+      </header>
+    );
+  }
 }
 
-export default header;
+export default Header;
--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "prettier";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
 //Redux
 import { createStore, applyMiddleware, compose } from "redux";
@@ -27,7 +27,6 @@ ReactDOM.render(
   </Provider>,
   rootElement
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
